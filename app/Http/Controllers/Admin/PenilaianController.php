c41e6cd7d579f03dfb6eada9f279cfb460c3c20a
Moch. Iqbal Rosyidi <iqbal.rosyidi.32@gmail.com>
2023-12-03 02:07:49 +0700

    tambah func normalisasi

--- app/Http/Controllers/Admin/PenilaianController.php
+++ app/Http/Controllers/Admin/PenilaianController.php
@@ -17,8 +17,43 @@ class PenilaianController extends Controller
         $datakriteria = Kriteria::all();
         $datasubkriteria = SubKriteria::all();
 
+        // ini adalah cara untuk menggunakan fungsi dari normalisasi (biarkan dia private karena hanya untuk di file ini)
+        $hasil_normalisasi = $this->Normalisasi();
+        // untuk pengecekan hasilnya
+        dd($hasil_normalisasi);
+
         // dd($data_pegawai, $data_kriteria);
         return view('admin.penilaian.create', compact('datapegawai', 'datakriteria', 'datasubkriteria'));
+    }
+
+    // kasih inputan tanggal
+    private function Normalisasi() {
+        // get data
+        $getKriteria = Kriteria::all();
+        $daftar_penilaian = Penilaian::where('bulan', '2023-11')
+                                    ->join('kriteria', 'kriteria.id', '=', 'penilaian.id_kriteria')
+                                    ->join('data_pegawai', 'data_pegawai.id', '=', 'penilaian.id_pegawai')
+                                    ->get();
+
+        // Looping setiap kriteria
+        foreach ($getKriteria as $kriteria) {
+            // mencari nilai normalisasi
+            foreach ($daftar_penilaian as $key => $butir_penilaian) {
+                // cek benefit atau cost
+                if ($butir_penilaian->jenis == 'Benefit') {
+                    // mencari nilai max
+                    $max = $daftar_penilaian->where('kriteria', $kriteria->kriteria)->max('nilai');
+                    // hasil
+                    $result[$key] = $butir_penilaian->nilai / $max;
+                }else if ($butir_penilaian->jenis == 'Cost'){
+                    // mencari nilai min
+                    $min = $daftar_penilaian->where('kriteria', $kriteria->kriteria)->min('nilai');
+                    // hasil
+                    $result[$key] = $butir_penilaian->nilai / $min;
+                }
+            }
+        }
+        return $result;
     }
 
     public function store(Request $request)
